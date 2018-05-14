f81766bd71072566d599d8e18b4efc4b9911d946
github username <github email address>
2018-05-14 10:42:47 -0700

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,35 @@
 def addition(num1, num2)
+  answer = num1 + num2
+  return answer
 end
 
 def subtraction(num1, num2)
+  answer = num1 - num2
+  return answer
 end
 
 def division(num1, num2)
+  answer = num1 / num2
+  return answer
 end
 
 def multiplication(num1, num2)
+  answer = num1 * num2
+  return answer
 end
 
 def modulo(num1, num2)
+  answer = num1 % num2
+  return answer
 end
 
 def square_root(num)
+  answer = Math.sqrt(num)
+  return answer
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  answer = num1 + (( num2 * num3 ) / num4)
+  return answer
 end
